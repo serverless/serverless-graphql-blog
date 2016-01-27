45aff3c46a363e614db9f8500b07da35cbe9fecc
Kevin Old <kevin@kevinold.com>
2016-01-27 11:34:25 -0600

    add GraphQL/GraphiQL introspectionQuery support

--- blog/lib/index.js
+++ blog/lib/index.js
@@ -3,8 +3,16 @@ import Schema from './schema';
 
 export function runGraphQL(event, cb) {
 
-  graphql(Schema, event.query).then( function(result) {
-    console.log('RESULT: ', result);
+  let query = event.query;
+
+  // patch to allow queries from GraphiQL
+  // like the initial introspectionQuery
+  if (event.query && event.query.hasOwnProperty('query')) {
+    query = event.query.query.replace("\n", ' ', "g");
+  }
+
+  graphql(Schema, query).then( function(result) {
+    //console.log('RESULT: ', result);
     return cb(null, result);
   });
 
